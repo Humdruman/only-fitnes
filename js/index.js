d4e75393398f7d6f61d79924e8ebc892570ab17b
Depeche <lightzerling@gmail.com>
2020-08-25 00:26:34 +0300

    many fixes 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+  $('.main-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    appendDots: $('.main-append-dots'),
+  });
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 760) {
+      $('.main-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        appendDots: $('.main-append-dots'),
+      });
+  
+    } else {
+    }
+  
+  })
   $('.reviews-photo-slider').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    dots: true,
+    appendDots: $('.reviews-photo-append-dots'),
   });
 })
