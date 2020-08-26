0b63967297f7500f88b3cc204e1998ba74642a41
Depeche <lightzerling@gmail.com>
2020-08-27 01:37:17 +0300

    99%

--- js/index.js
+++ js/index.js
@@ -1,11 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-  $('.main-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    appendDots: $('.main-append-dots'),
-  });
+  if (window.innerWidth > 760) {
+    $('.main-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      appendDots: $('.main-append-dots'),
+    });
+  
+    $('.reviews-photo-slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      dots: true,
+      appendDots: $('.reviews-photo-append-dots'),
+    });
+    
+  } else {
+    
+    $('.workouts-cards').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      appendDots: $('.workouts-append-dots'),
+    });
+  
+    $('.reviews-photo-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+    });
+  
+    $('.reviews-review-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      appendDots: $('.reviews-review-append-dots'),
+      arrows: false
+    });
+  }
+  
+  
   window.addEventListener("resize", () => {
     if (window.innerWidth > 760) {
       $('.main-slider').slick({
@@ -15,16 +53,35 @@ document.addEventListener('DOMContentLoaded', () => {
         dots: true,
         appendDots: $('.main-append-dots'),
       });
+      
+      $('.workouts-cards').slick('unslick');
   
+      $('.reviews-photo-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        appendDots: $('.reviews-photo-append-dots'),
+      });
+      
     } else {
-    }
+      $('.main-slider').slick('unslick');
+      
+      $('.workouts-cards').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        appendDots: $('.workouts-append-dots'),
+      });
   
+      $('.reviews-photo-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        appendDots: $('.reviews-photo-append-dots'),
+      });
+    }
   })
-  $('.reviews-photo-slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
-    appendDots: $('.reviews-photo-append-dots'),
-  });
 })
